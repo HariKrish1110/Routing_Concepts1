6807f63142685243f5fcc5afacd0c28ba4d39067
HariKrish1110 <hari11101997@gmail.com>
2021-06-09 18:30:08 +0530

    1 file modified

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,16 @@ import{Routes}from '@angular/router';
 import { from } from 'rxjs';
 import {ContactComponent} from './contact/contact.component';
 import {LoginComponent}from './login/login.component';
+import { OrderdetailsComponent } from './login/orderdetails/orderdetails.component';
 import{UserComponent}from './user/user.component';
 
 export const Approute:Routes=[
   
   {path:'login',component: LoginComponent},
   {path:'user',component:UserComponent},
-  {path:'contact',component:ContactComponent}
+  {path:'contact',component:ContactComponent},
+  {path:'orderdetails',component:OrderdetailsComponent}
   
 ];
-export const routingcomponents=[LoginComponent,UserComponent,ContactComponent]
+export const routingcomponents=[LoginComponent,UserComponent,ContactComponent,OrderdetailsComponent]
 
